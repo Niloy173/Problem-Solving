16358fd19c9a7c5b48d9def244c8d5516b1eaf83
unknown <niloysaha173@gmail.com>
2022-07-30 04:45:28 +0600

    Max SunArray

--- Array/MaxSubArray.java
+++ Array/MaxSubArray.java
@@ -1,7 +1,4 @@
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class MaxSubArray {
@@ -9,59 +6,19 @@ public class MaxSubArray {
   public static void SubArray(int[] arr)
     {
 
-     
-      int start=0;
-      Map<Integer,int[]> myMap = new HashMap<>();
-
-      
+  
       for (int i = 0; i < arr.length; i++) {
-        
-        for (int j = i; j < arr.length; j++) {
-          
-         
-          int[] arr2 = Arrays.copyOfRange(arr, i, j+1);
-          myMap.put(start++, arr2);
-          
-          
-
+        System.out.print(arr[i]+" ");
+      }
+      int max;
+      for (int i = 0; i < arr.length; i++) {
+        max=arr[i];
+        for (int j = i+1; j < arr.length; j++) {
+          max = Math.max(max, arr[j]); 
+          System.out.print(max+" ");
         }
-
       }
 
-
-    
-
-
-        for(Map.Entry<Integer,int[]> map: myMap.entrySet())
-        {
-          // int key = map.getKey();
-          int[] new_arr = map.getValue().clone();
-          System.out.println(Arrays.toString(new_arr));
-          
-
-          // if(new_arr.length ==1)
-          // {
-          //   System.out.printf("%d ",new_arr[0]);
-          // }else{
-            
-          //   int max=0;
-          //   for (int i = 0; i < new_arr.length; i++) {
-              
-          //     if(new_arr[i] > max)
-          //     {
-          //       max = new_arr[i];
-          //     }
-           // }
-          
-            //System.out.printf("%d ",max);
-         // }
-        }
-        
-      
-
-     
-
-      
     }
       
 
